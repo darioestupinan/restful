08ba1a526bc4abd7ceeacf8f4adae1b8068bfc9b
darioestupinan <dario.estupinan@gmail.com>
2018-08-25 18:45:51 -0500

    adding db.json mock database and creating first route

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser'
 
+import {Pokemons} from "./routes/pokemon";
+
 class App{
 
     public app: express.Application;
+    public pokeRoutes: Pokemons = new Pokemons();
 
     constructor() {
         this.app = express();
         this.config();
+        this.pokeRoutes.routes(this.app);
     }
 
     private config(): void {
